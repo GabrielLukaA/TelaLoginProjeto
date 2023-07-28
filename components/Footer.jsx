e8148c23dff4bdbc577f9413791a1f925a529fad
GABRIEL LUKA DE AMARAL <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-07-27 21:55:55 -0300

    implementa login faltando dashed

---
+++ components/Footer.jsx
@@ -0,0 +1,66 @@
+export default function Footer() {
+  return (
+    <div className="mt-[313px] w-screen h-[250px] bg-[#00579D] pt-9">
+      <div className="flex flex-row gap-3 w-screen">
+        <div className="ml-12  w-16 h-14 bg-[#D9D9D9]"></div>
+        <p className="text-white text-3xl flex items-center h-[56px]">
+          Copyright © 2023 ((nome)). Todos os direitos reservados
+        </p>
+        <div className="w-1/3  flex flex-col gap-1 items-end mt-3  text-white font-semibold justify-center">
+          <div className="w-[136px] flex flex-col items-start">
+            <div className="flex gap-1">
+              <img
+                width="24px"
+                height="24px"
+                src="https://img.icons8.com/material/24/github.png"
+                className="h-6 flex items-center w-6"
+                alt="github"
+              />
+              <p className="flex items-center">BeckerMM</p>
+            </div>
+            <div className="flex gap-1">
+              <img
+                width="24px"
+                height="24px"
+                src="https://img.icons8.com/material/24/github.png"
+                className="h-6 flex items-center w-6"
+                alt="github"
+              />
+              <p className="flex items-center">DevMarquardt</p>
+            </div>
+            <div className="flex gap-1">
+              <img
+                width="24px"
+                height="24px"
+                src="https://img.icons8.com/material/24/github.png"
+                className="h-6 flex items-center w-6"
+                alt="github"
+              />
+              <p className="flex items-center">GabrielLukaA</p>
+            </div>
+            <div className="flex gap-1">
+              <img
+                width="24px"
+                height="24px"
+                src="https://img.icons8.com/material/24/github.png"
+                className="h-6 flex items-center w-6"
+                alt="github"
+              />
+              <p className="flex items-center">HeloisaFogaca1</p>
+            </div>
+            <div className="flex gap-1">
+              <img
+                width="24px"
+                height="24px"
+                src="https://img.icons8.com/material/24/github.png"
+                className="h-6 flex items-center w-6"
+                alt="github"
+              />
+              <p className="flex items-center">Jonatass06</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header(){
+    return (
+        <div className="w-screen h-24 bg-[#00579D] shadow-2xl"> </div>
+    )
+}
